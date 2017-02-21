60f83e6b6cb635268802cf5113c852b5ea787fbc
Ruslan Yupyn <r.yupyn@crossroadlabs.xyz>
2017-02-21 16:28:32 +0200

    added PathKit project files; added Cartfile for dependencies;

---
+++ PathKit/PathKit.h
@@ -0,0 +1,19 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by Ruslan Yupyn on 2/21/17.
+//  Copyright © 2017 CrossroadLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
