e896e11f9213a38644cbbea65bfecefc227fcf28
Ruslan Yupyn <r.yupyn@crossroadlabs.xyz>
2017-02-22 18:25:20 +0200

    added PathKitOSX shared scheme to project;

---
+++ PathKitOSX/PathKitOSX.h
@@ -0,0 +1,19 @@
+//
+//  PathKitOSX.h
+//  PathKitOSX
+//
+//  Created by Ruslan Yupyn on 2/22/17.
+//  Copyright © 2017 CrossroadLabs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PathKitOSX.
+FOUNDATION_EXPORT double PathKitOSXVersionNumber;
+
+//! Project version string for PathKitOSX.
+FOUNDATION_EXPORT const unsigned char PathKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKitOSX/PublicHeader.h>
+
+
